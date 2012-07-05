1fe8cec3520e045dd1eea26ac3924f5669ab9b49
André Renninger <renninger@marmalade.de>
2012-07-05 10:59:03 +0200

    remove screen output

--- copy_this/modules/marm_searchmanufacturers_oxsearch.php
+++ copy_this/modules/marm_searchmanufacturers_oxsearch.php
@@ -113,7 +113,6 @@ class marm_searchmanufacturers_oxsearch extends marm_searchmanufacturers_oxsearc
             $sSelect .= " order by {$sSortBy} ";
         }
 
-        echo $sSelect;
         return $sSelect;
     }
     
